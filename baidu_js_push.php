656051fd477a4ab2dfc0bc79ec0a2318cb60c83b
姬长信 <192666378@qq.com>
2017-10-17 17:01:08 +0800

    Update baidu_js_push.php

--- baidu_js_push.php
+++ baidu_js_push.php
@@ -4,7 +4,7 @@
 
 
     &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?>
-Copyright &copy; <a href="//blog.isoyu.com/">BLOG.ISOYU.COM</a><br><a href="#" target="_blank">豫ICP备15034003号-1</a> <span style="display:none;"><script language="javascript" type="text/javascript" src="https://js.users.51.la/18705002.js"></script></span>  <a href="//blog.isoyu.com/sitemap.xml">Sitemap</a> 由<a target="_blank" href="http:////typecho.org">Typecho</a> 强力驱动.<br><a target="_blank" href="https://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=2_ri6e3t7ejs45uqqvW4tLY" style="text-decoration:none;"><img src="https://rescdn.qqmail.com/zh_CN/htmledition/images/function/qm_open/ico_mailme_12.png"/></a>
+Copyright &copy; <a href="//blog.isoyu.com/">BLOG.ISOYU.COM</a><br><a href="#" target="_blank">豫ICP备15034003号-1</a> <span style="display:none;"><script language="javascript" type="text/javascript" src="https://js.users.51.la/18705002.js"></script></span>  <a href="//blog.isoyu.com/sitemap.xml">Sitemap</a> 由<a target="_blank" href="http:////typecho.org">Typecho</a> 强力驱动.<br>主题<a target="_blank" href="https://github.com/insoxin/inso">isoyu</a><a target="_blank" href="https://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=2_ri6e3t7ejs45uqqvW4tLY" style="text-decoration:none;"><img src="https://rescdn.qqmail.com/zh_CN/htmledition/images/function/qm_open/ico_mailme_12.png"/></a>
 <script>
 <script>
 (function(){
